872ecb2cc619e3e15696545239d11a303bede4f3
tuitige <tige.young@gmail.com>
2025-03-31 13:58:45 -0700

    update ng components and html

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TrainingComponent } from './pages/training/training.component';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    TrainingComponent
+  ],
   imports: [
     BrowserModule,
-    AppComponent,
     RouterModule.forRoot([
       { path: '', redirectTo: '/home', pathMatch: 'full' },
-      { path: 'home', component: AppComponent },
-      { path: 'training', loadComponent: () => 
-        import('./pages/training/training.component').then(m => m.TrainingComponent) },
-      { path: 'learning', loadComponent: () => 
-        import('./pages/learning/learning.component').then(m => m.LearningComponent) },
-      { path: 'about', loadComponent: () => 
-        import('./pages/about/about.component').then(m => m.AboutComponent) }
+      { path: 'home', component: HomeComponent },
+      { path: 'training', component: TrainingComponent }
     ])
   ],
   providers: [],
