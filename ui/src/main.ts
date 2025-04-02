6b6128990de8f267e0c76f494613a467d422b896
tuitige <tige.young@gmail.com>
2025-04-02 14:32:44 -0700

    Update Angular and Amplify configurations for compatibility

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,17 +1,9 @@
 import { Amplify } from 'aws-amplify';
-import { AuthClass } from '@aws-amplify/auth';
-import { cognitoUserPoolsTokenProvider } from '@aws-amplify/auth/cognito';
-
-const auth = new AuthClass({
-  tokenProvider: cognitoUserPoolsTokenProvider
-});
 
 Amplify.configure({
   Auth: {
-    Cognito: {
+    region: 'us-west-2',
     userPoolId: 'us-west-2_shE3zxrwp',
-    userPoolWebClientId: '6c1anji9n56kt4bmqtp2a0c8kb',
-      region: 'us-west-2'
-    }
+    userPoolWebClientId: '6c1anji9n56kt4bmqtp2a0c8kb'
   }
 });
