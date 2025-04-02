8c96facab9e1a2711bc2c24dddcef2471e643793
tuitige <tige.young@gmail.com>
2025-04-02 14:03:35 -0700

    fix dep conflicts and update build

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,14 +1,17 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { Amplify } from 'aws-amplify';
-import { AppModule } from './app/app.module';
+import { AuthClass } from '@aws-amplify/auth';
+import { cognitoUserPoolsTokenProvider } from '@aws-amplify/auth/cognito';
+
+const auth = new AuthClass({
+  tokenProvider: cognitoUserPoolsTokenProvider
+});
 
 Amplify.configure({
   Auth: {
-    region: 'us-west-2',
+    Cognito: {
     userPoolId: 'us-west-2_shE3zxrwp',
-    userPoolWebClientId: '6c1anji9n56kt4bmqtp2a0c8kb'
+    userPoolWebClientId: '6c1anji9n56kt4bmqtp2a0c8kb',
+      region: 'us-west-2'
+    }
   }
 });
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
