a0ae667b37fe9fdfe932ecde6d9fac6ade25952d
tuitige <tige.young@gmail.com>
2025-04-14 13:43:35 +1200

    fix rawresponse issue for translate

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,19 +3,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { TrainingComponent } from './pages/training/training.component';
+import { HeaderComponent } from './components/header/header.component';
 import { TranslationService } from './services/translation.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrainingComponent
+    TrainingComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule  // Make sure this is included
   ],
   providers: [TranslationService],
   bootstrap: [AppComponent]
