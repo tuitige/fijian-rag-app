4e00cbb533e4ff85572c72b2c40460b3fd62b3cd
tuitige <tige.young@gmail.com>
2025-04-16 08:27:17 +1200

    update routes for module-verify

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { TrainingComponent } from './pages/training/training.component';
+import { VerifyModuleComponent } from './pages/verify-module/verify-module.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'training', component: TrainingComponent },
+  { path: 'verify-module/:title', component: VerifyModuleComponent },
   { path: '**', redirectTo: '/home' }  // Catch-all route for 404s
 ];
