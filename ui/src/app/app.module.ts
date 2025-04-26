59651845aaf4fd4fecb44ce9961084edc3af6695
tuitige <tige.young@gmail.com>
2025-04-27 09:43:24 +1200

    update env url

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,38 +1,37 @@
-// src/app/app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { TrainingComponent } from './pages/training/training.component';
-import { TranslationService } from './services/translation.service';
-import { VerifyModuleComponent } from './pages/verify-module/verify-module.component';
+import { HomeComponent } from './pages/home/home.component';
 import { PagesComponent } from './pages/pages/pages.component';
 import { ArticleReviewComponent } from './pages/article-review/article-review.component';
+import { TrainingComponent } from './pages/training/training.component';
+import { ModuleListComponent } from './pages/module-list/module-list.component';
 import { LearnComponent } from './pages/learn/learn.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    VerifyModuleComponent,
+    HomeComponent,
     PagesComponent,
+    ArticleReviewComponent,
+    TrainingComponent,
+    ModuleListComponent,
     LearnComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
-    AmplifyAuthenticatorModule,
-    ArticleReviewComponent,
-    TrainingComponent,
-    CommonModule,
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
-  providers: [TranslationService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
