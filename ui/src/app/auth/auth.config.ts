3eb07d8c6c71037e72162a46a685d98928bf8963
tuitige <tige.young@gmail.com>
2025-05-28 12:40:00 +1200

    fix cognito login config

--- ui/src/app/auth/auth.config.ts
+++ ui/src/app/auth/auth.config.ts
@@ -2,7 +2,8 @@ import { PassedInitialConfig } from 'angular-auth-oidc-client';
 
 export const authConfig: PassedInitialConfig = {
   config: {
-    authority: 'https://auth.fijian-ai.org',
+    //authority: 'https://auth.fijian-ai.org',
+    authority: 'https://cognito-idp.us-west-2.amazonaws.com/us-west-2_shE3zxrwp',
     redirectUrl: window.location.origin,
     postLogoutRedirectUri: window.location.origin,
     clientId: '4pvrvr5jf8h9bvi59asmlbdjcp',
