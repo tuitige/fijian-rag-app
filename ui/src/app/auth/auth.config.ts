cb456fe1cc3c56d955902c9b241b6a6a7b1b0d17
tuitige <tige.young@gmail.com>
2025-05-28 14:00:27 +1200

    fix cognito login config

--- ui/src/app/auth/auth.config.ts
+++ ui/src/app/auth/auth.config.ts
@@ -7,21 +7,6 @@ export const authConfig: PassedInitialConfig = {
     clientId: '4pvrvr5jf8h9bvi59asmlbdjcp',
     scope: 'email openid profile',
     responseType: 'code'
-};
+  };
 
-/*
-export const authConfig: PassedInitialConfig = {
-  config: {
-    //authority: 'https://auth.fijian-ai.org',
-    authority: 'https://cognito-idp.us-west-2.amazonaws.com/us-west-2_shE3zxrwp',
-    redirectUrl: window.location.origin,
-    postLogoutRedirectUri: window.location.origin,
-    clientId: '4pvrvr5jf8h9bvi59asmlbdjcp',
-    scope: 'openid profile email',
-    responseType: 'code',
-    silentRenew: true,
-    renewTimeBeforeTokenExpiresInSeconds: 60,
-    useRefreshToken: true
-  }
 }
-*/
