c47ca1dc19905944d9fa4602a47b8b7eb2192917
tuitige <tige.young@gmail.com>
2025-05-28 13:59:07 +1200

    fix cognito login config

--- ui/src/app/auth/auth.config.ts
+++ ui/src/app/auth/auth.config.ts
@@ -2,16 +2,11 @@ import { PassedInitialConfig } from 'angular-auth-oidc-client';
 
 export const authConfig: PassedInitialConfig = {
   config: {
-    authority: 'https://auth.fijian-ai.org',
-    redirectUrl: window.location.origin,
-    postLogoutRedirectUri: window.location.origin,
+    authority: 'https://cognito-idp.us-west-2.amazonaws.com/us-west-2_shE3zxrwp',
+    redirectUrl: 'https://fijian-ai.org',
     clientId: '4pvrvr5jf8h9bvi59asmlbdjcp',
-    scope: 'openid profile email',
-    responseType: 'code',
-    silentRenew: true,
-    renewTimeBeforeTokenExpiresInSeconds: 60,
-    useRefreshToken: true
-  }
+    scope: 'email openid profile',
+    responseType: 'code'
 };
 
 /*
